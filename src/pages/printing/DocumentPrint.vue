9f0d1a096f43a2562bd6932fb1c68f8291b2efe4
caxerx <caxerx@caxerx.com>
2019-03-10 06:42:24 +0800

    Document Section, Video Consultation Screen

--- src/pages/printing/DocumentPrint.vue
+++ src/pages/printing/DocumentPrint.vue
@@ -1,70 +1,14 @@
 <template>
-  <container>
-    <div slot="content">
-      <section ref="print">
-        <logo/>
-        <v-card>
-          <v-card-text>
-            <p class="text-sm-left"></p>
-            <p class="text-lg-left">
-              <v-menu
-                :close-on-content-click="false"
-                v-model="menu"
-                :nudge-right="40"
-                lazy
-                transition="scale-transition"
-                offset-y
-                full-width
-                max-width="290px"
-                min-width="290px"
-              >
-                <v-text-field
-                  slot="activator"
-                  v-model="date"
-                  full-width="360"
-                  label="Date "
-                  hint="YYYY-MM-DD format"
-                  persistent-hint
-                  prepend-icon="event"
-                  readonly
-                ></v-text-field>
-                <v-date-picker v-model="date" no-title @input="menu = false"></v-date-picker>
-              </v-menu>
-            </p>
-            <h1 style="    text-align: center;">Referral Letter</h1>
-            <v-flex xs12>
-             <v-textarea solo rows="10" name="input-7-4" label="content.."></v-textarea>
-            
-            </v-flex>
-            <p class="text-xs-right"  style="margin-top:200px">Signature</p>
-          </v-card-text>
-        </v-card>
-        <div class="no-print">
-          <v-btn @click="print()">PRINT</v-btn>
-        </div>
-      </section>
-    </div>
-  </container>
+  <v-container>
+    
+  </v-container>
 </template>
 
 <script>
-import Logo from "@/components/Logo.vue";
-import Container from "@/components/Container.vue";
-
 export default {
-  components: {
-    Logo,
-    Container
-  },
-  data: () => ({
-    menu: false,
-    date: new Date().toISOString().substr(0, 10)
-  }),
+  components: {},
+  data: () => ({}),
 
-  methods: {
-    print() {
-      this.$print(this.$refs.print);
-    }
-  }
+  methods: {}
 };
 </script>
--- src/pages/printing/DocumentTemplate.vue
+++ src/pages/printing/DocumentTemplate.vue
@@ -1,29 +1,36 @@
 <template>
   <v-container>
-    <v-card class="mb-2">
-      <v-card-title class="headline">Placeholder List</v-card-title>
-      <v-card-text>
-        <v-layout row wrap>
-          <v-flex xs12 md6 xl3>{DoctorName} - Doctor Name</v-flex>
-          <v-flex xs12 md6 xl3>{DoctorTel} - Doctor Telephone</v-flex>
-          <v-flex xs12 md6 xl3>{DocContent} - Document Content</v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-    <v-card>
-      <v-toolbar flat>
-        <v-toolbar-title>Template Editor</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon @click="preview">
-          <v-icon>visibility</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>save</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <editor mode="wysiwyg" :options="editorOptions" v-model="docTemplate" ref="tuiEditor"></editor>
-    </v-card>
-    <div v-html="previewHtml"></div>
+    <v-layout row wrap>
+      <v-flex md6>
+        <v-card class="mb-2">
+          <v-card-title class="headline">Placeholder List</v-card-title>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs12 lg6>{DoctorName} - Doctor Name</v-flex>
+              <v-flex xs12 lg6>{DoctorTel} - Doctor Telephone</v-flex>
+              <v-flex xs12 lg6>{DocContent} - Document Content</v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>Template Editor</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>save</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <editor mode="wysiwyg" :options="editorOptions" v-model="docTemplate" ref="tuiEditor"></editor>
+        </v-card>
+      </v-flex>
+      <v-divider vertical class="mx-2"></v-divider>
+      <v-flex md5>
+        <span class="headline">Preview</span>
+          <v-sheet color="white" class="pa-3">
+            <div v-html="previewHtml" style="width:100%"></div>
+          </v-sheet>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -68,6 +75,11 @@ export default {
       ]
     }
   }),
+  watch: {
+    docTemplate() {
+      this.preview();
+    }
+  },
   methods: {
     preview() {
       this.previewHtml = this.$refs.tuiEditor.invoke("getHtml");
@@ -92,3 +104,10 @@ export default {
   }
 };
 </script>
+
+<style>
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
