9b0644f18d78371647384d10ce907b4c5ac273de
larylo1025 <lolary1s30@gmail.com>
2019-04-23 17:46:10 +0800

    added placeholder for calendar and profile

---
+++ src/page/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <full-screen-container>
+    <div style="height:100%; width:100%" class="pa-4">
+      <h3 class="headline mb --text">Profile Setting</h3>
+    </div>
+  </full-screen-container>
+</template>
+<script>
+import FullScreenContainer from "@/component/FullScreenContainer.vue";
+
+export default {
+  components: {
+    "full-screen-container": FullScreenContainer
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {}
+};
+</script>
+
