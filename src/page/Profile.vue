b645e00d0c5b2973b4a15e125f0bd0ed95a5bbf0
larylo1025 <lolary1s30@gmail.com>
2019-04-30 21:05:34 +0800

    profile upload avatar

--- src/page/Profile.vue
+++ src/page/Profile.vue
@@ -1,8 +1,8 @@
 <template>
-  <full-screen-container>
+  <full-screen-container v-if="doctor">
     <div style="height:100%; width:100%; overflow-y: scroll" class="pa-4">
       <h3 class="headline mb --text">Profile Setting</h3>
-      <v-layout v-if="doctor" row>
+      <v-layout row>
         <!-- Doctor information -->
         <v-flex xs8 pr-3>
           <v-container grid-list-xs>
@@ -101,33 +101,95 @@
         <!-- Avatar -->
         <v-flex xs4 pl-5 py-3 pr-3>
           <v-layout column>
-            <v-img v-if="doctor.avatar" aspect-ratio="1" :src="doctor.avatar"></v-img>
+            <v-img
+              class="mb-4"
+              v-if="doctor.avatar"
+              aspect-ratio="1"
+              :src="`https://dsapi.1lo.li/assets/avatars/${doctor.avatar}`"
+            ></v-img>
             <v-img
               class="mb-4"
               v-else
               aspect-ratio="1"
               :src="require('@/asset/avatar-placeholder.png')"
             ></v-img>
-            <v-btn color="primary">Change avatar</v-btn>
-            <v-btn color="primary">Change password</v-btn>
+            <v-btn color="primary" @click="changeAvatar">Change avatar</v-btn>
+            <v-btn color="primary" @click="changePassword">Change password</v-btn>
           </v-layout>
         </v-flex>
       </v-layout>
-      <v-layout v-else>Loading...</v-layout>
     </div>
+    <!-- Change avatar dialog -->
+    <v-dialog v-model="avatarDialog" max-width="600">
+      <v-card class="pa-3">
+        <h1 class="headline">Change avatar</h1>
+        <v-layout row align-center>
+          <v-text-field v-if="avatarFile" :value="avatarFile.name" readonly label="File name"></v-text-field>
+          <v-text-field v-else placeholder="No files selected" readonly label="File name"></v-text-field>
+          <upload-btn
+            accept="image/png, image/jpeg"
+            class="pa-0"
+            title="Select file"
+            :fileChangedCallback="avatarFileChanged"
+          ></upload-btn>
+          <v-btn :disabled="!avatarFile" color="primary" @click="uploadAvatar">
+            <v-progress-circular indeterminate v-if="avatarUploading"></v-progress-circular>
+            <template v-else>Upload</template>
+          </v-btn>
+        </v-layout>
+      </v-card>
+    </v-dialog>
+    <!-- Change password dialog -->
+    <v-dialog v-model="passwordDialog" max-width="400">
+      <v-card class="pa-4">
+        <h1 class="headline mb-2">Change password</h1>
+        <v-layout column>
+          <v-text-field label="Current password" type="password" v-model="oldPassword"></v-text-field>
+          <v-text-field label="New password" type="password" v-model="newPassword"></v-text-field>
+          <v-text-field
+            class="mb-2"
+            label="Confirm password"
+            type="password"
+            v-model="confirmPassword"
+            :rules="[checkPassword]"
+          ></v-text-field>
+          <v-btn :disabled="!passwordMatch" color="primary">Change password</v-btn>
+        </v-layout>
+      </v-card>
+    </v-dialog>
+    <v-snackbar v-model="snackbar">{{ snackbarMessage }}</v-snackbar>
+  </full-screen-container>
+  <full-screen-container v-else>
+    <v-layout align-center justify-center fill-height>
+      <v-progress-circular width="5" size="50" color="primary" indeterminate></v-progress-circular>
+    </v-layout>
   </full-screen-container>
 </template>
 <script>
 import FullScreenContainer from "@/component/FullScreenContainer.vue";
+import UploadButton from "vuetify-upload-button";
 import gql from "graphql-tag";
 
 export default {
   components: {
-    "full-screen-container": FullScreenContainer
+    "full-screen-container": FullScreenContainer,
+    "upload-btn": UploadButton
   },
   data() {
     return {
+      doctor: null,
+      snackbar: false,
+      snackbarMessage: "",
       dobMenu: false,
+      // TODO: Avatar
+      avatarDialog: false,
+      avatarFile: null,
+      avatarUploading: false,
+      // TODO: Change password
+      passwordDialog: false,
+      oldPassword: "",
+      newPassword: "",
+      confirmPassword: "",
       specialties: [
         { text: "General Pratice", value: "general_practice" },
         { text: "Cardiology", value: "cardiology" },
@@ -139,39 +201,99 @@ export default {
         { text: "Spanish", value: "spanish" },
         { text: "Mandarin", value: "Mandarin" },
         { text: "Cantonese", value: "Cantonese" }
-      ],
-      doctor: null
-      // Basic info
-      // name: "",
-      // gender: "",
-      // email: "",
-      // phoneNo: "",
-      // dob: "",
-      // hkid: "",
-      // specialty: "",
-      // language: "",
-      // academic: "",
-      // experience: "",
-      // workplace: null
-      // TODO: Avatar
-      // TODO: Change password
+      ]
     };
   },
+  computed: {
+    passwordMatch() {
+      return !!this.newPassword && this.newPassword === this.confirmPassword;
+    }
+  },
   methods: {
+    changeAvatar() {
+      this.avatarDialog = true;
+    },
+    avatarFileChanged(file) {
+      // console.log(file);
+      this.avatarFile = file;
+    },
+    uploadAvatar() {
+      const vars = {
+        id: this.$store.state.userId,
+        avatar: this.avatarFile
+      };
+      // console.log(vars);
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation uploadDoctorAvatar($id: ID!, $avatar: Upload!) {
+              uploadDoctorAvatar(doctorId: $id, file: $avatar) {
+                success
+                message
+                mimetype
+                filename
+              }
+            }
+          `,
+          variables: vars
+        })
+        .then(({ data: { uploadDoctorAvatar: { message } } }) => {
+          // console.log(result);
+          this.snackbar = true;
+          this.snackbarMessage = message;
+        })
+        .catch(err => {
+          this.snackbar = true;
+          this.snackbarMessage = "Error uploading avatar";
+          console.log(err);
+        })
+        .finally(() => {
+          this.avatarDialog = false;
+        });
+    },
+    changePassword() {
+      this.passwordDialog = true;
+    },
+    checkPassword() {
+      if (this.newPassword === this.confirmPassword) {
+        return true;
+      } else {
+        return "New password and confirm password does not match";
+      }
+    },
     saveProfile() {
       this.$apollo.mutate({
         mutation: gql`
           mutation saveProfile($id: ID!, $data: DoctorInput!) {
             editDoctor(id: $id, data: $data) {
               id
+              name
+              dob
+              email
+              gender
+              phoneNo
+              hkid
+              language
+              specialty
+              experience
+              academic
             }
           }
         `,
-        variables() {
-          return {
-            id: 0,
-            data: {}
-          };
+        variables: {
+          id: this.$store.state.userId,
+          data: {
+            name: this.doctor.name,
+            dob: this.doctor.dob,
+            email: this.doctor.email,
+            gender: this.doctor.gender,
+            phoneNo: this.doctor.phoneNo,
+            hkid: this.doctor.hkid,
+            language: this.doctor.language,
+            specialty: this.doctor.specialty,
+            experience: this.doctor.experience,
+            academic: this.doctor.academic
+          }
         }
       });
     }
@@ -181,6 +303,7 @@ export default {
       query: gql`
         query getDoctor($doctorId: ID!) {
           doctor(id: $doctorId) {
+            avatar
             name
             gender
             email
